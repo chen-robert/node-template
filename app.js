769f0a2094297cece0f416483c25694d45cb427d
Robert <robertchen@live.com>
2019-10-24 15:49:23 +0000

    Moved http declaration into separate file

---
+++ app.js
@@ -0,0 +1,20 @@
+global.__rootdir = __dirname;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const autoprefixer = require('express-autoprefixer');
+const lessMiddleware = require('less-middleware');
+const cookieSession = require("cookie-session");
+
+const app = express();
+app.set("trust proxy", "127.0.0.1");
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+const staticPath = __dirname + '/public';
+app.use(lessMiddleware(staticPath));
+app.use(autoprefixer({browsers: ["last 3 versions", "> 1%"], cascade: false}));
+app.use(express.static(staticPath));
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-global.__rootdir = __dirname;
-
-const express = require("express");
-const bodyParser = require("body-parser");
-const autoprefixer = require('express-autoprefixer');
-const lessMiddleware = require('less-middleware');
-const cookieSession = require("cookie-session");
+const app = require(__dirname + "/app");
 
 const PORT = process.env.PORT || 3000;
-
-const app = express();
-app.set("trust proxy", "127.0.0.1");
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({extended: false}));
-
-const staticPath = __dirname + '/public';
-app.use(lessMiddleware(staticPath));
-app.use(autoprefixer({browsers: ["last 3 versions", "> 1%"], cascade: false}));
-app.use(express.static(staticPath));
 
 app.listen(PORT, () => console.log(`Started server at port ${PORT}`));
