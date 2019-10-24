367c92502f017de169032a60b56d53cacd444200
Robert <robertchen@live.com>
2019-10-24 17:19:14 +0000

    Added auto formatter

--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
-npm init && npm i express body-parser express-autoprefixer less-middleware cookie-session && npm i --save-dev nodemon
+npm init \
+  && npm i express body-parser express-autoprefixer less-middleware cookie-session \
+  && npm i --save-dev nodemon eslint \
+    eslint-config-standard \
+    eslint-plugin-import \
+    eslint-plugin-node \
+    eslint-plugin-promise \
+    eslint-plugin-standard \
+  && npx npm-add-script -k start -v "node index.js" \
+  && npx npm-add-script -k dev -v "nodemon index.js" \
+  && npx npm-add-script -k format -v "eslint \"**/*.js\"" 
+
